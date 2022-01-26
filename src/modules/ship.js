658b81e0c21fb47489b26024866953599654ea1c
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-01-26 15:03:37 +0800

    feat: add orientation property to ship objects
    - also added a changeOrientation() method for ship objects

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -6,24 +6,30 @@ const shipProto = {
             throw new Error("Cannot hit ship at forbidden index");
         }
     },
+
     isSunk() {
         for (let i = 0; i < this.length; i++) {
             if (!this.hit[i]) return false;
         }
         return true;
     },
+
     reset() {
         this.hit.fill(false);
     },
 };
 
 function shipFactory(length) {
-    const ship = Object.create(shipProto);
+    return Object.assign(Object.create(shipProto), {
+        length,
+        hit: new Array(length).fill(false),
 
-    ship.length = length;
-    ship.hit = new Array(length).fill(false);
-
-    return ship;
+        // TODO: create tests for these properties
+        orientation: "h",
+        changeOrientation() {
+            this.orientation = this.orientation === "h" ? "v" : "h";
+        },
+    });
 }
 
 export { shipFactory };
