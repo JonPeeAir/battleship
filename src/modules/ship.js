a77716e9c5f9d3dc5fbe4deaf650fd4ace7d5330
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-01-27 16:05:42 +0800

    refactor: change shipFactory to createShip and add an isShip function
    - also moved the changeOrientation method to the ship prototype
    - this is because a board that is set has no access to changeOrientation anymore so there is no need to remove it entirely

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,6 +1,9 @@
 const shipProto = {
+    changeOrientation() {
+        this.orientation = this.orientation === "h" ? "v" : "h";
+    },
     hitShip(index) {
-        if (index < this.length) {
+        if (index >= 0 && index < this.length) {
             this.hit[index] = true;
         } else {
             throw new Error("Cannot hit ship at forbidden index");
@@ -17,16 +20,21 @@ const shipProto = {
     },
 };
 
-function shipFactory(length) {
+function createShip(length) {
     return Object.assign(Object.create(shipProto), {
         length,
         hit: new Array(length).fill(false),
         orientation: "h",
-        // This is NOT a prototype method because we want to remove this when a board is set
-        changeOrientation() {
-            this.orientation = this.orientation === "h" ? "v" : "h";
-        },
     });
 }
 
-export { shipFactory };
+function isShip(ship) {
+    return (
+        ship.hasOwnProperty("length") &&
+        ship.hasOwnProperty("hit") &&
+        ship.hasOwnProperty("orientation") &&
+        Object.getPrototypeOf(ship) === shipProto
+    );
+}
+
+export { createShip, isShip };
