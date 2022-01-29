88821d6a35a3c2efb432466162b4bafdff83ea2a
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-01-29 16:11:43 +0800

    feat/refactor: add new methods and properties to gameboard and more
    - gameboard now has methods for adding and editing ships around the board
    - gameboard now also has a method to check if the board has lost
    - the gameboard module can now also be imported in two ways just like the ship module
    
    - this commit also introduces a new object called BattleShip
    - a BattleShip is basically a Ship object with data about its location on the board
    - basically, by adding a Ship to a board, it becomes a BattleShip
    
    - this commit also adds lodash as a dependency
    - because many of the new methods rely heavily on array checks and manipulation
    - and lodash has a bunch of handy array functions
    
    - in turn, this commit also adds and updates gameboard.test.js to accomodate all these changes

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,80 +1,301 @@
+import { intersectionWith as intersect, isEqual, findIndex } from "lodash";
 import events from "events";
+import { isShip } from "./ship";
 
-const gameboardProto = {
-    width: 10,
-    height: 10,
-
-    receiveAttack(xPos, yPos) {
-        this.playArea[xPos][yPos] = true;
-        this.checkAndUpdateShips(xPos, yPos);
-
-        this.eventEmitter.emit("boardUpdate");
-    },
-
-    checkAndUpdateShips(xPos, yPos) {
-        this.ships.forEach(shipObj => {
-            if (shipObj.ship.orientation === "h") {
-                if (shipObj.yPos === yPos) {
-                    if (
-                        xPos >= shipObj.xPos &&
-                        xPos < shipObj.xPos + shipObj.ship.length
-                    ) {
-                        shipObj.ship.hitShip(xPos - shipObj.xPos);
-                    }
-                }
-            } else if (shipObj.ship.orientation === "v") {
-                if (shipObj.xPos === xPos) {
-                    if (
-                        yPos >= shipObj.yPos &&
-                        yPos < shipObj.yPos + shipObj.ship.length
-                    ) {
-                        shipObj.ship.hitShip(yPos - shipObj.yPos);
-                    }
-                }
-            }
-        });
-    },
-
-    checkIfAllShipsHaveSunk() {
-        for (let i = 0; i < gameboard.ships.length; i++) {
-            if (!gameboard.ships[i].ship.isSunk()) {
-                console.log("There are still ships left");
-                return false;
+const battleShipProto = {
+    get coords() {
+        const coords = [];
+        for (let i = 0; i < this.ship.length; i++) {
+            if (this.ship.orientation === "h") {
+                coords.push([this.headX + i, this.headY]);
+            } else if (this.ship.orientation === "v") {
+                coords.push([this.headX, this.headY + i]);
             }
         }
-
-        console.log("All ships have sunk");
-        return true;
-    },
-
-    set() {
-        // this permanently sets the gameboard so that it becomes uneditable
-        delete this.putShip;
-        this.ships.forEach(shipObj => {
-            delete shipObj.ship.changeOrientation;
-        });
+        return coords;
     },
 };
 
-function gameboardFactory() {
-    const grid = new Array(gameboardProto.height);
-    for (let i = 0; i < grid.length; i++) {
-        grid[i] = new Array(gameboardProto.width).fill(false);
-    }
-
-    const gameboard = Object.assign(Object.create(gameboardProto), {
-        playArea: grid,
-        ships: [],
-        putShip(ship, xPos, yPos) {
-            const shipObj = { ship, xPos, yPos };
-            this.ships.push(shipObj);
-        },
-        eventEmitter: new events.EventEmitter(),
-    });
-
-    gameboard.eventEmitter.on("boardUpdate", gameboard.checkIfAllShipsHaveSunk);
-
-    return gameboard;
+function createBattleShip(ship, headX, headY) {
+    // Basically a ship object with data about its location on the board
+    const battleShip = { ship, headX, headY };
+    return Object.assign(Object.create(battleShipProto), battleShip);
 }
 
-export { gameboardFactory };
+const boardProto = (() => {
+    // Private helper method
+    function shipCollidesWithOtherShips(battleShip) {
+        for (let i = 0; i < this.ships.length; i++) {
+            const thisCoords = battleShip.coords;
+            const shipsCoords = this.ships[i].coords;
+            if (intersect(thisCoords, shipsCoords, isEqual).length > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Private helper method
+    function shipCrossesTheBorders(battleShip) {
+        const coords = battleShip.coords;
+        for (let i = 0; i < coords.length; i++) {
+            if (coords[i].some(v => v < 0 || v >= this.size)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Private helper method
+    function updateShips(xPos, yPos) {
+        this.ships.forEach(battleShip => {
+            const attackHits = battleShip.coords.some(v =>
+                isEqual(v, [xPos, yPos]),
+            );
+            const hitIndex = findIndex(battleShip.coords, item =>
+                isEqual(item, [xPos, yPos]),
+            );
+
+            if (attackHits) {
+                battleShip.ship.hitShip(hitIndex);
+                // Alert subscribers of ship update if the attack hits
+                this.event.emit("shipUpdate");
+            }
+        });
+    }
+
+    return {
+        setBoard() {
+            this.set = true;
+        },
+
+        unsetBoard() {
+            this.set = false;
+        },
+
+        putShip(ship, headX, headY) {
+            if (this.set) {
+                throw new Error(
+                    "Cannot put anymore ships when the board is set",
+                );
+            }
+            if (!isShip(ship)) {
+                throw new Error("Invalid ship passed to Gameboard.putShip");
+            }
+
+            const battleShip = createBattleShip(ship, headX, headY);
+
+            // Function.call allows us to pass the context of "this" to the Function
+            // We do this because shipCrossesTheBorder() and shipCollidesWithOtherShips() need to know the context of "this"
+            if (shipCrossesTheBorders.call(this, battleShip)) {
+                throw new Error(
+                    "Ship found outside the borders, unable to put ship",
+                );
+            }
+            if (shipCollidesWithOtherShips.call(this, battleShip)) {
+                throw new Error(
+                    "Ship collides with other ships, unable to put ship",
+                );
+            }
+
+            // Push the battleShip if no errors were thrown
+            this.ships.push(battleShip);
+
+            // Return ship index if putting battleShip was successful
+            return this.ships.length - 1;
+        },
+
+        moveShip(shipIndex, headX, headY) {
+            if (this.set) {
+                throw new Error("Cannot move ship when the board is set");
+            }
+            if (shipIndex < 0 || shipIndex >= this.ships.length) {
+                throw new Error(
+                    `Ship at index [${shipIndex}] does not exist, unable to move ship`,
+                );
+            }
+
+            // Pop the ship from ships array and store into battleShip
+            const battleShip = this.ships.splice(shipIndex, 1)[0];
+
+            // Save a reference to the old head coords
+            const oldX = battleShip.headX;
+            const oldY = battleShip.headY;
+
+            // Update the battleShip with the new head coords
+            battleShip.headX = headX;
+            battleShip.headY = headY;
+
+            // Test collision
+            if (shipCrossesTheBorders.call(this, battleShip)) {
+                // New coords collide with borders so revert back to old coords
+                battleShip.headX = oldX;
+                battleShip.headY = oldY;
+
+                // And put the battleShip back in the array
+                this.ships.splice(shipIndex, 0, battleShip);
+
+                throw new Error(
+                    "Ship found outside the borders, unable to put ship",
+                );
+            }
+
+            if (shipCollidesWithOtherShips.call(this, battleShip)) {
+                // New coords collide with other ships so revert back to old coords
+                battleShip.headX = oldX;
+                battleShip.headY = oldY;
+
+                // And put the battleShip back in the array
+                this.ships.splice(shipIndex, 0, battleShip);
+
+                throw new Error(
+                    "Ship collides with other ships, unable to move ship",
+                );
+            }
+
+            // If no errors were thrown, we can safely put moved battleShip back into array
+            this.ships.splice(shipIndex, 0, battleShip);
+
+            // And return ship index if moving the ship was successful
+            return shipIndex;
+        },
+
+        rotateShip(shipIndex) {
+            if (this.set) {
+                throw new Error("Cannot rotate ship when the board is set");
+            }
+            if (shipIndex < 0 || shipIndex >= this.ships.length) {
+                throw new Error(
+                    `Ship at index [${shipIndex}] does not exist, unable to rotate ship`,
+                );
+            }
+
+            // Pop the ship from ships array and store into shipObj
+            const battleShip = this.ships.splice(shipIndex, 1)[0];
+
+            // Attempt to rotate Ship
+            battleShip.ship.changeOrientation();
+
+            // Test collision
+            if (shipCrossesTheBorders.call(this, battleShip)) {
+                // Put ship back to its previous orientation
+                battleShip.ship.changeOrientation();
+                // Then put it back in the array
+                this.ships.splice(shipIndex, 0, battleShip);
+
+                throw new Error(
+                    "Ship found outside the borders, unable to rotate ship",
+                );
+            }
+            if (shipCollidesWithOtherShips.call(this, battleShip)) {
+                // Put ship back to its previous orientation
+                battleShip.ship.changeOrientation();
+                // Then put it back in the array
+                this.ships.splice(shipIndex, 0, battleShip);
+
+                throw new Error(
+                    "Ship collides with other ships, unable to rotate ship",
+                );
+            }
+
+            // If no errors were thrown, we can safely put rotated battleShip back into array
+            this.ships.splice(shipIndex, 0, battleShip);
+
+            // Return ship index if rotating the ship was successful
+            return shipIndex;
+        },
+
+        receiveAttack(xPos, yPos) {
+            if (!this.set) {
+                throw new Error("Cannot receive attack unless board is set");
+            }
+            if (xPos < 0 || xPos >= this.size) {
+                throw new Error("Invalid xPos");
+            }
+            if (yPos < 0 || yPos >= this.size) {
+                throw new Error("Invalid yPos");
+            }
+            if (this.playArea[xPos][yPos] === true) {
+                throw new Error("This spot has already been hit");
+            }
+
+            // If no errors were thrown, safely update the board
+            this.playArea[xPos][yPos] = true;
+
+            // Then alert subscribers of a board update
+            this.event.emit("boardUpdate");
+
+            // Then update the ships if the attack hits
+            updateShips.call(this, xPos, yPos);
+        },
+
+        hasLost() {
+            if (!this.set) {
+                throw new Error(
+                    "Cannot determine win status unless board is set",
+                );
+            }
+            return this.ships.every(battleShip => battleShip.ship.isSunk());
+        },
+
+        reset() {
+            this.unsetBoard();
+            this.playArea.forEach(column => {
+                column.fill(false);
+            });
+            this.ships.length = 0;
+        },
+    };
+})();
+
+function createGameboard() {
+    const size = 10;
+    let playArea = new Array(size);
+    for (let i = 0; i < playArea.length; i++) {
+        playArea[i] = new Array(size).fill(false);
+    }
+    let setValue = false;
+    let event = new events.EventEmitter();
+
+    const gameboard = Object.assign(Object.create(boardProto), {
+        size,
+        playArea,
+        ships: [],
+    });
+
+    Object.defineProperties(gameboard, {
+        set: {
+            get: () => setValue,
+            set: newValue => {
+                if (newValue === true || newValue === false) {
+                    setValue = newValue;
+                } else {
+                    throw new Error("Invalid assignment to set");
+                }
+            },
+        },
+        event: {
+            get: () => event,
+            set: newEvent => (event = newEvent),
+        },
+    });
+
+    return Object.freeze(gameboard);
+}
+
+function isGameboard(gameboard) {
+    return (
+        gameboard.hasOwnProperty("size") &&
+        gameboard.hasOwnProperty("playArea") &&
+        gameboard.hasOwnProperty("ships") &&
+        gameboard.hasOwnProperty("event") &&
+        gameboard.hasOwnProperty("set") &&
+        Object.getPrototypeOf(gameboard) === boardProto
+    );
+}
+
+// This allows external modules to import individual functions
+export { createGameboard, isGameboard };
+
+// This allows external modules to import all functions as a single object
+export default { createGameboard, isGameboard };
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,19 +1,299 @@
-import { gameboardFactory } from "../src/modules/gameboard";
+import Gameboard from "../src/modules/gameboard";
 
-test("gameboardFactory returns an object", () => {
-    expect(gameboardFactory()).toBeInstanceOf(Object);
+// For ship related gameboard methods
+import { createShip } from "../src/modules/ship";
+
+test("createGameboard returns an object", () => {
+    expect(Gameboard.createGameboard()).toBeInstanceOf(Object);
+});
+
+test("createGameboard returns a Gameboard object", () => {
+    const gameboard = Gameboard.createGameboard();
+
+    // Test instance properties
+    expect(gameboard).toHaveProperty("size");
+    expect(gameboard).toHaveProperty("playArea");
+    expect(gameboard).toHaveProperty("ships");
+    expect(gameboard).toHaveProperty("set");
+    expect(gameboard).toHaveProperty("event");
+
+    // Test prototype properties
+    const gbProto = Object.getPrototypeOf(gameboard);
+    expect(gbProto).toHaveProperty("putShip");
+    expect(gbProto).toHaveProperty("moveShip");
+    expect(gbProto).toHaveProperty("rotateShip");
+    expect(gbProto).toHaveProperty("setBoard");
+    expect(gbProto).toHaveProperty("unsetBoard");
+    expect(gbProto).toHaveProperty("receiveAttack");
+    expect(gbProto).toHaveProperty("hasLost");
+    expect(gbProto).toHaveProperty("reset");
+
+    // Also test it on the isGameboard function
+    expect(Gameboard.isGameboard(gameboard)).toBe(true);
+});
+
+test("isGameboard returns true when given a gameboard", () => {
+    const gameboard = Gameboard.createGameboard();
+    expect(Gameboard.isGameboard(gameboard)).toBe(true);
+});
+
+test("isGameboard returns false when given a non-gameboard", () => {
+    const nonGameboard = {
+        playArea: [[false]],
+        ships: [],
+        set: false,
+        event: {},
+    };
+    expect(Gameboard.isGameboard(nonGameboard)).toBe(false);
 });
 
 describe("Gameboard object related tests", () => {
-    test("Gameboard has width of 10", () => {
-        const gameboard = gameboardFactory();
+    let gameboard;
 
-        expect(gameboard.width).toBe(10);
+    beforeEach(() => {
+        gameboard = Gameboard.createGameboard();
     });
 
-    test("Gameboard has height of 10", () => {
-        const gameboard = gameboardFactory();
+    describe("Gameboard.size tests", () => {
+        test("Gameboard.size is set to 10", () => {
+            expect(gameboard.size).toBe(10);
+        });
 
-        expect(gameboard.height).toBe(10);
+        test("Gameboard.size is non-writable", () => {
+            expect(() => (gameboard.size = 0)).toThrow();
+        });
+    });
+
+    describe("Gameboard.playArea tests", () => {
+        test("Gameboard.playArea is initialized to a 10x10 array filled with false values", () => {
+            expect(gameboard.playArea.length).toBe(10);
+
+            gameboard.playArea.forEach(column => {
+                expect(column.length).toBe(10);
+
+                column.forEach(area => {
+                    expect(area).toBe(false);
+                });
+            });
+        });
+
+        test("Gameboard.prototype.receiveAttack() correctly updates the playArea", () => {
+            gameboard.setBoard();
+            gameboard.receiveAttack(1, 1);
+            expect(gameboard.playArea[1][1]).toBe(true);
+        });
+    });
+
+    describe("Gameboard.ships tests", () => {
+        test("Gameboard.ships is initialized to an empty array", () => {
+            expect(gameboard.ships).toEqual(new Array());
+        });
+
+        test("Gameboard.prototype.putShip() adds a ship to Gameboard.ships together with the coords of its head element", () => {
+            const ship = createShip(4);
+            gameboard.putShip(ship, 1, 1);
+
+            expect(gameboard.ships[0]).toMatchObject({
+                ship: {
+                    length: 4,
+                    hit: [false, false, false, false],
+                    orientation: "h",
+                },
+                headX: 1,
+                headY: 1,
+            });
+        });
+
+        test("Gameboard.prototype.receiveAttack() updates Gameboard.ships if the attack hits", () => {
+            const ship = createShip(3);
+            gameboard.putShip(ship, 0, 0);
+            gameboard.setBoard();
+
+            gameboard.receiveAttack(0, 0);
+            expect(gameboard.ships[0].ship.hit[0]).toBe(true);
+        });
+
+        test("Gameboard.prototype.receiveAttack() makes no changes to Gameboard.ships if the attack doesn't hit", () => {
+            const ship = createShip(3);
+            gameboard.putShip(ship, 0, 0);
+            gameboard.setBoard();
+
+            gameboard.receiveAttack(0, 1);
+            expect(gameboard.ships[0].ship.hit[0]).toBe(false);
+        });
+
+        test("Gameboard.prototype.hasLost() returns true when all ships have sunk", () => {
+            const ship = createShip(2);
+            gameboard.putShip(ship, 0, 0);
+            gameboard.setBoard();
+
+            gameboard.receiveAttack(0, 0);
+            gameboard.receiveAttack(1, 0);
+
+            expect(gameboard.hasLost()).toBe(true);
+        });
+
+        test("Gameboard.prototype.hasLost() returns false when not all ships have sunk", () => {
+            const ship = createShip(2);
+            gameboard.putShip(ship, 0, 0);
+            gameboard.setBoard();
+
+            gameboard.receiveAttack(0, 0);
+
+            expect(gameboard.hasLost()).toBe(false);
+        });
+    });
+
+    describe("Gameboard.set tests", () => {
+        test("Gameboard.set is initialized to false", () => {
+            expect(gameboard.set).toBe(false);
+        });
+
+        test("Gameboard.prototype.setBoard() sets Gameboard.set to true", () => {
+            gameboard.setBoard();
+            expect(gameboard.set).toBe(true);
+        });
+
+        test("Gameboard.prototype.unsetBoard() sets Gameboard.set to false", () => {
+            gameboard.setBoard();
+            expect(gameboard.set).toBe(true);
+
+            gameboard.unsetBoard();
+            expect(gameboard.set).toBe(false);
+        });
+
+        test("Gameboard.prototype.putShip() throws an error when trying to put a ship on a set board", () => {
+            const ship = createShip(3);
+            gameboard.setBoard();
+            expect(() => gameboard.putShip(ship, 0, 0)).toThrow();
+        });
+
+        test("Gameboard.prototype.moveShip() throws an error when trying to move a ship on a set board", () => {
+            const shipIndex = gameboard.putShip(createShip(4), 0, 0);
+            gameboard.setBoard();
+            expect(() => gameboard.moveShip(shipIndex, 0, 1)).toThrow();
+        });
+
+        test("Gameboard.prototype.rotateShip() throws an error when trying to rotate a ship on a set board", () => {
+            const shipIndex = gameboard.putShip(createShip(4), 0, 0);
+            gameboard.setBoard();
+            expect(() => gameboard.rotateShip(shipIndex)).toThrow();
+        });
+
+        test("Gameboard.prototype.receiveAttack() throws an error when the board is not set", () => {
+            expect(() => gameboard.receiveAttack(1, 1)).toThrow();
+            expect(gameboard.playArea[1][1]).toBe(false);
+
+            gameboard.setBoard();
+
+            gameboard.receiveAttack(1, 1);
+            expect(gameboard.playArea[1][1]).toBe(true);
+        });
+
+        test("Gameboard.prototype.hasLost() throws an error when the board is not set", () => {
+            expect(() => gameboard.hasLost()).toThrow();
+            gameboard.setBoard();
+            expect(() => gameboard.hasLost()).not.toThrow();
+        });
+    });
+
+    describe("Gameboard.event object tests", () => {
+        test("Gameboard.event is an EventEmitter object", () => {
+            expect(gameboard.event.constructor.name).toBe("EventEmitter");
+        });
+
+        test("No two Gameboards have the same event object", () => {
+            const gameboard2 = Gameboard.createGameboard();
+            expect(gameboard.event == gameboard2.event).toBe(false);
+        });
+    });
+
+    describe("Other Gameboard.prototype tests", () => {
+        test("Gameboard.prototype.putShip() throws an error when passed in a non-ship", () => {
+            const nonShip = {
+                length: 4,
+                hit: new Array(4).fill(false),
+                orientation: "h",
+            };
+
+            expect(() => gameboard.putShip(nonShip, 0, 0)).toThrow();
+        });
+
+        test("Gameboard.prototype.putShip() throws an error when passed in coords outside of the board", () => {
+            const ship = createShip(3);
+            expect(() => gameboard.putShip(ship, -1, 0)).toThrow();
+            expect(() => gameboard.putShip(ship, 0, -1)).toThrow();
+            expect(() => gameboard.putShip(ship, -1, -1)).toThrow();
+            expect(() => gameboard.putShip(ship, 10, 9)).toThrow();
+            expect(() => gameboard.putShip(ship, 9, 10)).toThrow();
+            expect(() => gameboard.putShip(ship, 10, 10)).toThrow();
+        });
+
+        test("Gameboard.prototype.putShip() throws an error when passed in coords that interfere with other ships", () => {
+            const ship = createShip(3);
+            gameboard.putShip(ship, 0, 0);
+
+            const ship2 = createShip(3);
+            expect(() => gameboard.putShip(ship2, 1, 0)).toThrow();
+        });
+
+        test("Gameboard.prototype.moveShip() throws an error when passed in an invalid index", () => {
+            expect(() => gameboard.moveShip(0, 0, 0)).toThrow();
+        });
+
+        test("Gameboard.prototype.moveShip() throws an error when passed in coords that cross outside the board", () => {
+            const shipIndex = gameboard.putShip(createShip(3), 0, 0);
+            expect(() => gameboard.moveShip(shipIndex, 8, 0)).toThrow();
+        });
+
+        test("Gameboard.prototype.moveShip() throws an error when passed in coords that interfere with other ships", () => {
+            const ship1 = gameboard.putShip(createShip(3), 0, 0);
+            const ship2 = gameboard.putShip(createShip(3), 0, 1);
+            expect(() => gameboard.moveShip(ship1, 2, 1)).toThrow();
+        });
+
+        test("Gameboard.prototype.rotateShip() throws an error when passed in an invalid index", () => {
+            expect(() => gameboard.rotateShip(0)).toThrow();
+        });
+
+        test("Gameboard.prototype.rotateShip() throws an error if rotating the ship causes it to cross outside the board", () => {
+            const shipIndex = gameboard.putShip(createShip(3), 0, 9);
+            expect(() => gameboard.rotateShip(shipIndex)).toThrow();
+        });
+
+        test("Gameboard.prototype.rotateShip() throws an error if rotating the ship interferes with other ships", () => {
+            const ship1 = gameboard.putShip(createShip(3), 0, 0);
+            const ship2 = gameboard.putShip(createShip(3), 0, 1);
+            expect(() => gameboard.rotateShip(ship1)).toThrow();
+        });
+
+        test("Gameboard.prototype.reset() factory resets a gameboard", () => {
+            // Mess around with gameboard
+            const ship = createShip(3);
+            const ship2 = createShip(4);
+            gameboard.putShip(ship, 0, 0);
+            gameboard.putShip(ship2, 1, 1);
+            gameboard.setBoard();
+            gameboard.receiveAttack(0, 0);
+
+            // Reset gameboard
+            gameboard.reset();
+
+            // Check Gameboard.playArea
+            expect(gameboard.playArea.length).toBe(10);
+            gameboard.playArea.forEach(column => {
+                expect(column.length).toBe(10);
+
+                column.forEach(area => {
+                    expect(area).toBe(false);
+                });
+            });
+
+            // Check Gameboard.ships
+            expect(gameboard.ships).toEqual(new Array());
+
+            // Check Gameboard.set
+            expect(gameboard.set).toBe(false);
+        });
     });
 });
