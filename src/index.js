f725d3ad345fd332382637d1df135c0d305bd9e5
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-02-22 08:47:47 +0800

    feat: create a main menu
    - the main menu is where the user gets to pick which gamemode they would like to play
    
    NOTE:
    - I haven't linked the menu buttons to their designated gamemodes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 // Module imports
 import { createPlayer } from "./modules/factories/player";
+import { createMainMenu } from "./modules/ui/mainMenu/mainMenu";
 import { renderSetupPhaseFor } from "./modules/ui/setupPhase/setupPhase";
 
 // Asset imports
 import "./styles.css";
 
-const p1 = createPlayer("p1");
-const setupPhase = renderSetupPhaseFor(p1);
+// const p1 = createPlayer("p1");
+// const setupPhase = renderSetupPhaseFor(p1);
+
+const mainMenu = createMainMenu();
 
 const main = document.getElementById("main");
-main.appendChild(setupPhase);
+main.appendChild(mainMenu);
---
+++ src/modules/ui/mainMenu/mainMenu.js
@@ -0,0 +1,49 @@
+function createMainMenu() {
+    const mainMenu = document.createElement("div");
+    mainMenu.id = "main-menu";
+    mainMenu.classList.add("main-menu");
+
+    const menuInstructions = document.createElement("p");
+    menuInstructions.id = "menu-instructions";
+    menuInstructions.classList.add("menu-instructions");
+    menuInstructions.textContent = "Choose A Gamemode";
+
+    const localGameContainer = document.createElement("div");
+    localGameContainer.id = "local-game-container";
+    localGameContainer.classList.add("gamemode-container");
+
+    const localGameBtn = document.createElement("button");
+    localGameBtn.id = "local-game-btn";
+    localGameBtn.classList.add("menu-btn");
+    localGameBtn.innerHTML = `<i class="bi bi-person"></i> <br> ------- <br> <i class="bi bi-person"></i>`;
+
+    const localGameLabel = document.createElement("p");
+    localGameLabel.id = "local-game-label";
+    localGameLabel.classList.add("mode-label");
+    localGameLabel.textContent = "VS. PLAYER";
+
+    localGameContainer.append(localGameBtn, localGameLabel);
+
+    const botGameContainer = document.createElement("div");
+    botGameContainer.id = "bot-game-container";
+    botGameContainer.classList.add("gamemode-container");
+
+    const botGameBtn = document.createElement("button");
+    botGameBtn.id = "bot-game-btn";
+    botGameBtn.classList.add("menu-btn");
+    botGameBtn.innerHTML = `<i class="bi bi-person"></i> <br> ------- <br> <i class="bi bi-robot"></i>`;
+
+    const botGameLabel = document.createElement("p");
+    botGameLabel.id = "bot-game-label";
+    botGameLabel.classList.add("mode-label");
+    botGameLabel.textContent = "VS. BOT";
+
+    botGameContainer.append(botGameBtn, botGameLabel);
+
+    mainMenu.append(menuInstructions, localGameContainer, botGameContainer);
+
+    return mainMenu;
+}
+
+export { createMainMenu };
+export default { createMainMenu };
